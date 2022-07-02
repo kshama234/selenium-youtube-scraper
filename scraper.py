cdcbe3ac4e7b3225965610dd0d94cee22cb80fb0
Kshama Singhal <61552757+kshama234@users.noreply.github.com>
2022-07-02 08:22:28 +0000

    added youtube scraper

--- scraper.py
+++ scraper.py
@@ -1,1 +1,11 @@
-print('Hello World!')
+import requests
+
+Youtube_trending_url="https://www.youtube.com/feed/trending"
+
+response=requests.get(Youtube_trending_url)
+
+print('Status Code:',response.status_code)
+print(len(response.text))
+
+with open ('trending.html','w') as file:
+  file.write(response.text)
